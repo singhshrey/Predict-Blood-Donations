b17f9aaf15ed6abcc98c0353e03b5984f09e5072
Shrey Surendra Singh <36826903+singhshrey@users.noreply.github.com>
2019-06-29 19:31:52 +0530

    Update Blood Donation Classification.py

--- Blood Donation Classification.py
+++ Blood Donation Classification.py
@@ -21,7 +21,6 @@ X_train = pca.fit_transform(X_train)
 X_test = pca.transform(X_test)
 explained_variance = pca.explained_variance_ratio_
 
-
 #Artificial Neural Network
 
 import keras
@@ -29,7 +28,6 @@ from keras.models import Sequential
 from keras.layers import Dense
 
 classifier = Sequential()
-
 
 classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 3 ))
 
@@ -42,10 +40,3 @@ classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = [
 classifier.fit(X_train, y_train, batch_size = 8, epochs = 150)
 
 y_pred = classifier.predict(X_test)
-
-import csv
-myData = y_pred
-myFile = open('testset_results.csv','w')
-with myFile:
-    writer = csv.writer(myFile)
-    writer.writerows(myData)
